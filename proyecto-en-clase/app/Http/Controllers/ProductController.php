be8a4b7eb1fa27be0cd0bf5ab92b3f49eb733d66
Maria Sofia Oyola Lozano <moyola@unab.edu.co>
2026-08-20 11:37:41 -0500

    Rutas basicas

---
+++ proyecto-en-clase/app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+   public function index ()
+    {
+        return "Show Products";
+    }
+
+    public function create ()
+    {
+        return "Create Product";
+    }
+
+    public function show ($idProduct)
+    {
+        return "Show Product: $idProduct";
+    }
+}
