7bf9c7b8a68ab023b0c7f98233c71d823d61f822
Maria Sofia Oyola Lozano <moyola@unab.edu.co>
2026-09-10 11:44:15 -0500

    modelos

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class CartItem extends Model
 {
-    protected $table = 'products';
+    protected $table = 'cart_items';
     protected $primaryKey = 'id';
 }
